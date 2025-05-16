e42f5264bd21b7ab514da55f3d957ad35bd71358
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-05-16 23:25:17 +0000

    Messing around.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -90,7 +90,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
                 { name: "right", keys: ["ArrowRight", "d", "D"] },
                 { name: "jump", keys: ["Space"] },
             ]}>
-                <Canvas shadows camera={{ fov: 45 }}>
+                <Canvas shadows camera={{ fov: 45 }} style={{height: '100%', width: '100%'}}>
                     <Sky sunPosition={[100, 20, 100]} />
                     <ambientLight intensity={0.3} />
                     <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
